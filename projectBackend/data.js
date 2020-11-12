bd7ac8616ba28d6bde2bc832f5c5299ba803f917
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2020-11-12 14:12:32 -0600

    added user file with mongo DB

--- projectBackend/data.js
+++ projectBackend/data.js
@@ -1,4 +1,23 @@
+import bcrypt from 'bcryptjs';
 const data = {
+
+  users: [
+
+    {
+      name:'Sahib',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234',8),
+      isAdmin: true,
+
+    },
+    {
+      name:'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234',8),
+      isAdmin: false,
+    },
+  ],
+
     products: [
       {
         _id: '1',
