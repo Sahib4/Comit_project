4e95564a6eeabf06c45f9a8e8a4f0a92006c908c
Sahib4 <68962355+Sahib4@users.noreply.github.com>
2020-11-13 17:31:55 -0600

    Created signin for backed

--- projectBackend/data.js
+++ projectBackend/data.js
@@ -20,8 +20,8 @@ const data = {
 
     products: [
       {
-        _id: '1',
-        name: 'Nike Slim Shirt',
+       
+        name: ' Slim Shirt',
         category: 'Shirts',
         image: '/images/p1.jpg',
         price: 120,
@@ -32,8 +32,8 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '2',
-        name: 'Adidas Fit Shirt',
+       
+        name: ' Fit Shirt',
         category: 'Shirts',
         image: '/images/p2.jpg',
         price: 100,
@@ -44,7 +44,7 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '3',
+      
         name: 'Lacoste Free Shirt',
         category: 'Shirts',
         image: '/images/p3.jpg',
@@ -56,7 +56,7 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '4',
+        
         name: 'Nike Slim Pant',
         category: 'Pants',
         image: '/images/d1.jpg',
@@ -68,7 +68,7 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '5',
+        
         name: 'Puma Slim Pant',
         category: 'Pants',
         image: '/images/d2.jpg',
@@ -80,7 +80,7 @@ const data = {
         description: 'high quality product',
       },
       {
-        _id: '6',
+        
         name: 'Adidas Fit Pant',
         category: 'Pants',
         image: '/images/d3.jpg',
